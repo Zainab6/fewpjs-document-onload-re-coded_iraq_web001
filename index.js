87aad88644476b34fa1cbbfff3c52ede2fa4333a
Zainab Saud <zai99nb@gmail.com>
2020-05-26 12:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded",function(){
-  let par= document.getElementsById("text");
+  let par= document.getElementById("text");
   par.innerHTML="This is really cool!";
 })
